8f37992604a9e69f9b3114b76f58a659e6c21b0c
jarheadviceral <rob.jared.viceral@student.ateneo.edu>
2026-03-12 20:36:35 +0800

    fixed static files for render

--- retro_expense_tracker/settings.py
+++ retro_expense_tracker/settings.py
@@ -1,10 +1,18 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'retro-expense-tracker-dev-key'
+
+# SECURITY
+SECRET_KEY = 'replace-this-with-a-random-secret-key'
+
 DEBUG = True
-ALLOWED_HOSTS = ['https://expensetracker.onrender.com']
+
+ALLOWED_HOSTS = ["*"]
+
+
+# APPLICATIONS
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -13,31 +21,42 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'expenses',
 ]
 
+
+# MIDDLEWARE
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
+
 ROOT_URLCONF = 'retro_expense_tracker.urls'
+
+
+# TEMPLATES
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
+
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -45,7 +64,11 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'retro_expense_tracker.wsgi.application'
+
+
+# DATABASE
 
 DATABASES = {
     'default': {
@@ -54,13 +77,51 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = []
+
+# PASSWORD VALIDATION
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# INTERNATIONALIZATION
 
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'UTC'
+
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'expenses' / 'static']
+
+# STATIC FILES
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "expenses/static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+# WHITENOISE
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+# DEFAULT PRIMARY KEY
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
