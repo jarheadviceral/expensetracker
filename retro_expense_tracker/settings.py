5685bae2bbf632004af77e8dc5233a524a8f6cf8
jarheadviceral <rob.jared.viceral@student.ateneo.edu>
2026-03-16 20:02:22 +0800

    db reqs

--- retro_expense_tracker/settings.py
+++ retro_expense_tracker/settings.py
@@ -1,5 +1,13 @@
 from pathlib import Path
+import dj_database_url
 import os
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
+}
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
